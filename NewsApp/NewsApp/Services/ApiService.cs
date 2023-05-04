9f57bada95935a92770782c4b911db1170ee0bb5
asfend <56428592+codewithasfend@users.noreply.github.com>
2023-05-04 18:01:43 +0500

    News App in .NET MAUI

---
+++ NewsApp/NewsApp/Services/ApiService.cs
@@ -0,0 +1,20 @@
+﻿using NewsApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApp.Services
+{
+    public class ApiService
+    {
+        public async Task<Root> GetNews(string categoryName)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync($"https://gnews.io/api/v4/top-headlines?category={categoryName.ToLower()}&apikey=46ff0266d5da4de27d3f1ac066783ed8&lang=en");
+            return JsonConvert.DeserializeObject<Root>(response);
+        }
+    }
+}
